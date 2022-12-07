49bfb9ef4dd5c8988352ab8cb7009d6f12c3e0fc
ArtKuz1 <116139345+ArtKuz1@users.noreply.github.com>
2022-12-07 12:16:13 +0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+    $("#menu ul").hide();
+    $("#menu li span").click(function() {
+        $("#menu ul:visible").slideUp("normal");
+        if (($(this).next().is("ul")) && (!$(this).next().is(":visible"))) {
+            $(this).next().slideDown("normal");
+        }
+    });
+    });
